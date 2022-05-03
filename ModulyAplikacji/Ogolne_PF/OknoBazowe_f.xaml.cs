55862c964a9e0627d357e6098fa7390c3db0e85f
Sebastian Markiton <office@mktnsoft.pl>
2022-05-03 10:24:29 +0200

    Dodanie nowych formatek do projektu: - Wizyta_f - WizytyEwidencja_f - OknoBazowe_f - PacjenciEwidencja_f - Pacjent_f

---
+++ ModulyAplikacji/Ogolne_PF/OknoBazowe_f.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MediStoma3._0.ModulyAplikacji.Ogolne_PF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoBazowe_f.xaml
+    /// </summary>
+    public partial class OknoBazowe_f : Window
+    {
+        public OknoBazowe_f()
+        {
+            InitializeComponent();
+        }
+    }
+}
